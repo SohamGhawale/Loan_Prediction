6729da9a3d7fd994b9c8ac939d2549a02927441e
Soham Gahwale <ghawalesoham@gmail.com>
2026-08-21 09:05:59 +0530

    Update loan_prediction_deployement.py

--- loan_prediction_deployement.py
+++ loan_prediction_deployement.py
@@ -48,5 +48,5 @@ if st.button("Predict Loan"):
     for col in encoder:
       df[col] = encoder[col].transform(df[col])
 
-  prediction = model.predict(df)[0]
+  prediction = model.predict(df)
   st.success(f"Predicted Loan : {prediction[0]:,.2f}")
